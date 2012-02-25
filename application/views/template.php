825a2963339de79fd90b67279cd512061649499c
pashukhin <pashukhin@gmail.com>
2012-02-25 11:01:04 +0400

    Update application/views/template.php

--- application/views/template.php
+++ application/views/template.php
@@ -26,6 +26,18 @@ if(!$nt)
 		<!-- dataTables: http://datatables.net/index -->
 		<script src="/static/js/DataTables-1.9.0/media/js/jquery.dataTables.min.js"></script>
 		<script src="/static/js/voting.js"></script>
+		<script type="text/javascript">
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-29479698-1']);
+			_gaq.push(['_setDomainName', 'egolosovanie.ru']);
+			_gaq.push(['_setAllowLinker', true]);
+			_gaq.push(['_trackPageview']);
+			(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
+		</script>
 </head>
 <body>
 	<div id="header">
